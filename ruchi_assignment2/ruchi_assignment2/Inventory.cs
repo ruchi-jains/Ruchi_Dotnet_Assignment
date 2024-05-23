9b38cf881008649e5aa3a18b7b6039e0222c487a
ruchi-jains <143081269+ruchi-jains@users.noreply.github.com>
2024-05-23 12:58:40 +0530

    Add files via upload

---
+++ ruchi_assignment2/ruchi_assignment2/Inventory.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ruchi_assignment2
+{
+    public class Inventory
+    {
+        private List<Item> items;
+
+        public Inventory()
+        {
+            items = new List<Item>();
+        }
+        public void AddItem(Item item)
+        {
+            if (!(item == null || item.Id < 0 || string.IsNullOrEmpty(item.Name) || item.Price < 0 || item.Quantity < 0))
+            {
+                items.Add(item);
+                Console.WriteLine("Item added successfully.");
+            }
+            else
+            {
+                Console.WriteLine("make sure your entry is not null or negative");
+            }
+        }
+
+
+        public void DisplayItems()
+        {
+            if (items.Count == 0)
+            {
+                Console.WriteLine("No items in inventory.");
+            }
+            else
+            {
+                foreach (Item item in items)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+        public Item FindItemById(int id)
+        {
+            foreach (Item item in items)
+            {
+                if (item.Id == id)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        public void UpdateItem(int id, string name, double price, int quantity)
+        {
+            Item item = FindItemById(id);
+            if (item != null)
+            {
+                item.Name = name;
+                item.Price = price;
+                item.Quantity = quantity;
+                Console.WriteLine("Item updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Item not found.");
+            }
+        }
+
+        public void DeleteItem(int id)
+        {
+            Item item = FindItemById(id);
+            if (item == null  || !int.TryParse(Console.ReadLine(), out id) || id < 1 || id > items.Count)
+            {
+                Console.WriteLine("please enter valid input");
+            }
+            else
+            {
+                
+                items.Remove(item);
+                Console.WriteLine("Item deleted successfully.");
+            }
+        }
+    }
+}
---
+++ ruchi_assignment2/ruchi_assignment2/Item.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ruchi_assignment2
+{
+    public class Item
+    {
+        private int id;
+        private string name;
+        private double price;
+        private int quantity;
+
+        public Item(int id, string name, double price, int quantity)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+      
+         public int Id
+         {
+             get { return id; }
+             private set { id = value; }   //Encapsulation
+         }
+
+         public string Name
+         {
+             get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) == true)
+                {
+                    Console.WriteLine("Name cannot be null or empty");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+
+         public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Price must be greater than zero.");
+                }
+                else 
+                { 
+                    price = value;
+                }
+            }
+         }
+
+         public int Quantity
+         {
+             get { return quantity; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Quantity cannot be negative.");
+                }
+                else
+                {
+                    quantity = value;
+                }
+            }
+         }
+
+        public override string ToString()
+        {
+            return $"Item [ID={id}, Name={name}, Price={price}, Quantity={quantity}]";
+        }
+    }
+}
+
+
+
